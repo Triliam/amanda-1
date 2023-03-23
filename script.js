219153a78d91565cbcb8996b7e4f975389049a3d
Triliam <taisbuenovidotto@gmail.com>
2023-03-23 09:46:44 -0300

    amanda

---
+++ script.js
@@ -0,0 +1,25 @@
+var btn = document.querySelector(".no");
+var position
+btn.addEventListener("click", function() {
+position ? (position = 0) : (position = 350);
+btn.style.transform = `translate(${position}px,0px)`;
+btn.style.transition = "all 0.3s ease";
+});
+
+var btn = document.querySelector(".no");
+var position
+btn.addEventListener("mouseover", function() {
+position ? (position = 0) : (position = 350);
+btn.style.transform = `translate(${position}px,0px)`;
+btn.style.transition = "all 0.3s ease";
+});
+
+function tanks() {
+alert("tanks :)");
+}    
+
+function resize() {
+ window.resizeTo(400, 300); 
+}
+
+<button class="no" onclick="not()">não :(</button>
