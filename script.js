3b294e9afbf6dd89408778779784ad53473647cc
Triliam <taisbuenovidotto@gmail.com>
2023-03-24 12:12:31 -0300

    modificaçoes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var btn = document.querySelector(".no");
 var position
 btn.addEventListener("click", function() {
-position ? (position = 0) : (position = 350);
+position ? (position = 0) : (position = 150);
 btn.style.transform = `translate(${position}px,0px)`;
 btn.style.transition = "all 0.3s ease";
 });
@@ -9,17 +9,7 @@ btn.style.transition = "all 0.3s ease";
 var btn = document.querySelector(".no");
 var position
 btn.addEventListener("mouseover", function() {
-position ? (position = 0) : (position = 350);
+position ? (position = 0) : (position = 150);
 btn.style.transform = `translate(${position}px,0px)`;
 btn.style.transition = "all 0.3s ease";
 });
-
-function tanks() {
-alert("tanks :)");
-}    
-
-function resize() {
- window.resizeTo(400, 300); 
-}
-
-<button class="no" onclick="not()">não :(</button>
